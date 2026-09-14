14f9ffe164e285be686ee7aa17d7111bd823cb3f
perezzil <perezzil@OA-S142-24>
2026-09-14 16:00:03 -0400

    initial commit

---
+++ Perez_Angel_motion2026/Assets/scripts/vectorMath.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class vectorMath : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentMousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        DrawSquare(currentMousePosition, 5, Color.red, 0.5f);
+    }
+
+    float GetMag(Vector2 vector)
+    {
+        return Mathf.Sqrt(vector.x * vector.x + vector.y * vector.y);
+            
+    }
+
+    public static void DrawSquare(Vector2 centerPoint, float size, Color colour, float duration)
+    {
+        //top line
+        Vector2 Srartpoint = centerPoint + new Vector2(-size,size);
+        Vector2 endpoint = centerPoint + new Vector2(size,size);
+
+        Debug.DrawLine(endpoint, Srartpoint, colour, duration);
+
+        //left line
+         Srartpoint = centerPoint + new Vector2(-size, size);
+         endpoint = centerPoint + new Vector2(-size, -size);
+
+        Debug.DrawLine(endpoint, Srartpoint, colour, duration);
+
+        //right
+         Srartpoint = centerPoint + new Vector2(size, size);
+         endpoint = centerPoint + new Vector2(size, -size);
+
+        Debug.DrawLine(endpoint, Srartpoint, colour, duration);
+
+        //bottom
+        Srartpoint = centerPoint + new Vector2(size, -size);
+        endpoint = centerPoint + new Vector2(-size, -size);
+
+        Debug.DrawLine(endpoint, Srartpoint, colour, duration);
+
+
+
+
+    }
+}
+
+
